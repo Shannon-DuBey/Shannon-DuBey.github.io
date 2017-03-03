4cfdf5a1037bec3c35044bca4e3e30534a302197
SDuBey <sdubey@nexient.com>
2017-03-03 17:17:26 -0500

    many fixes to layout - adding fonts, adjusting about me sections, resolve resume text overlap issues

--- scripts/angularMain.js
+++ scripts/angularMain.js
@@ -4,10 +4,11 @@
 	app.controller('TabController', function(){
 		this.tab = 1;
 	    this.setTab = function(newValue){
-	      this.tab = newValue;
+	    	this.tab = newValue;
 	    };
+  
 	    this.isSet = function(tabName){
-	      return this.tab === tabName;
+	      	return this.tab === tabName;
 	    };
 	});
 
